75110ab2ae6650c1fbf442fe94afdf30c04d5401
victor3d <vics@itu.dk>
2025-04-24 11:10:06 +0200

    DER VIRKER AAAAAAAAAAA

--- screens/CameraPostScreen.js
+++ screens/CameraPostScreen.js
@@ -1,62 +1,17 @@
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import { useState, useRef } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import * as MediaLibrary from 'expo-media-library';
-import LoginScreen from './LoginScreen';
-
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { CameraView } from 'expo-camera';
 
 function CameraPostScreen({ navigation }) {
-  const [facing, setFacing] = useState('back');
-  const [permission, requestPermission] = useCameraPermissions();
-  const photoRef = useRef(null);
-  const [permissionResponse, requestMediaPermission] = MediaLibrary.usePermissions();
-
-  async function handlePermissions() {
-    if(permissionResponse.status !== 'granted') {
-      await requestMediaPermission();
-    } if (!permission.granted) {
-      await requestPermission();
-    }
-  }
-
-  if (!permission || !permissionResponse) {
-    // Camera permissions are still loading.
-    return <View />;
-  }
-
-  if (!permission.granted) {
-    // Camera permissions are not granted yet.
-    return (
-      <View style={styles.container}>
-        <Text style={styles.message}>We need your permission to show the camera</Text>
-        <Button onPress={handlePermissions} title="grant permission" />
-      </View>
-    );
-  }
-
-  function toggleCameraFacing() {
-    setFacing(current => (current === 'back' ? 'front' : 'back'));
-    console.log('Camera facing:', facing);
-  }
-
-  async function handleTakePhoto() {
-    const photo = photoRef.current?.takePictureAsync();
-    console.log(photo);
-    await MediaLibrary.saveToLibraryAsync(photo.uri);
-  }
-
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing} ref={photoRef}>
+      <CameraView style={styles.camera}>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Flip Camera</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => {
-            navigation.navigate('LoginScreen');
-            handleTakePhoto();
-            }}>
-            <Text style={styles.text} >Take Picture</Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('PostScreen')} // Navigate to PostScreen
+            >
+            <Text style={styles.text}>Take a picture</Text>
           </TouchableOpacity>
         </View>
       </CameraView>
@@ -71,27 +26,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
   },
-  message: {
-    textAlign: 'center',
-    paddingBottom: 10,
-  },
   camera: {
     flex: 1,
   },
   buttonContainer: {
     flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingBottom: 50,
   },
   button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
   },
   text: {
-    fontSize: 24,
-    fontWeight: 'bold',
     color: 'white',
+    fontSize: 32,
   },
 });
