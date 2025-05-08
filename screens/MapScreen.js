1939e101ec4a4c5f739b5803a41db8c878bb1d51
Jbidstrup <jobi@itu.dk>
2025-05-08 12:56:05 +0200

    Ulrik og paprika pins
    Der mangler grønne hjerter

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,95 +1,121 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet, Image, Alert } from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
-import * as Location from 'expo-location';
-
-import Favicon from '../assets/favicon.png';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Platform, Text, View, StyleSheet, Image, Alert } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import { Ionicons } from "@expo/vector-icons";
+import Favicon from "../assets/favicon.png";
 
 function MapScreen({ navigation }) {
-    const [errorMsg, setErrorMsg] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-    useEffect(() => {
-        async function handleRequestPermission() {
-          
-          let { status } = await Location.requestForegroundPermissionsAsync();
-          if (status !== 'granted') {
-            setErrorMsg('Permission to access location was denied');
-            return;
-          }
-        } handleRequestPermission();
-      }, []);
+  useEffect(() => {
+    async function handleRequestPermission() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+    }
+    handleRequestPermission();
+  }, []);
 
-      const CAPITAL_REGION = {
-        latitude: 55.6600747398742, 
-        longitude: 12.590989334477465,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-      }
-    
-      const ITU = {
-        latitude: 55.6600747398742, 
-        longitude: 12.590989334477465,
-      }
-      const ISMAGERIET = {
-        latitude: 55.66039040484868, 
-        longitude: 12.591769568288738,
-      }
+  const CAPITAL_REGION = {
+    latitude: 55.6600747398742,
+    longitude: 12.590989334477465,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
 
-      if (errorMsg) {
-        return (
-          <View style={styles.container}>
-            <Text>{errorMsg}</Text>
-          </View>
-        );
-      }
-
+  const ITU = {
+    latitude: 55.6600747398742,
+    longitude: 12.590989334477465,
+  };
+  const ISMAGERIET = {
+    latitude: 55.66039040484868,
+    longitude: 12.591769568288738,
+  };
+  const PAPRIKA = {
+    latitude: 55.6606334,
+    longitude: 12.5898479,
+  };
+  const ULRIK = {
+    latitude: 55.662011,
+    longitude: 12.5951199,
+  };
+  if (errorMsg) {
     return (
-        <View style={styles.container}>
-        <MapView 
-        style={styles.map}
-        initialRegion={CAPITAL_REGION}
-        showsUserLocation={true}
-        >
-          <Marker
-            coordinate={ITU}
-            title="ITU"
-            description="International IT University"
-            onPress={() => {
-                Alert.alert(
-                "ITU", // Title of the alert
-                "International IT University", // Message of the alert
-                [
-                    { text: "OK", onPress: () => console.log("OK Pressed") }, // Button
-                ]
-            );
-            }}
-          />
-          <Marker
-          coordinate={{latitude: ISMAGERIET.latitude, longitude: ISMAGERIET.longitude}}
-          title='Ismageriet'
-          description='i scream'
-          image={Favicon}
-          >
-            <Image source={Favicon} style={{width: 10, height: 10}} />
-          </Marker>
-  
-        </MapView>
+      <View style={styles.container}>
+        <Text>{errorMsg}</Text>
       </View>
-
-
     );
   }
 
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={CAPITAL_REGION}
+        showsUserLocation={true}
+      >
+        <Marker
+          coordinate={ITU}
+          title="ITU"
+          description="International IT University"
+          onPress={() => {
+            Alert.alert(
+              "ITU", // Title of the alert
+              "International IT University", // Message of the alert
+              [
+                { text: "OK", onPress: () => console.log("OK Pressed") }, // Button
+              ]
+            );
+          }}
+        />
+        <Marker
+          coordinate={{
+            latitude: ISMAGERIET.latitude,
+            longitude: ISMAGERIET.longitude,
+          }}
+          title="Ismageriet"
+          description="i scream"
+          image={Favicon}
+        >
+          <Image source={Favicon} style={{ width: 10, height: 10 }} />
+        </Marker>
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    map: {
-      width: '100%',
-      height: '100%',
-    },
-  });
+        <Marker
+          coordinate={PAPRIKA}
+          title="Paprika Solstråle"
+          description="Bananer, Oksekød, Yoghurt"
+        >
+          <View style={{ alignItems: "center", justifyContent: "center" }}>
+            <Ionicons name="heart" size={30} color="green" />
+          </View>
+        </Marker>
+
+        <Marker
+          coordinate={ULRIK}
+          title="Ulrik Wilbek"
+          description="Mælk, Laks, forårsløg"
+        >
+          <View style={{ alignItems: "center", justifyContent: "center" }}>
+            <Ionicons name="heart" size={30} color="green" />
+          </View>
+        </Marker>
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default MapScreen;
